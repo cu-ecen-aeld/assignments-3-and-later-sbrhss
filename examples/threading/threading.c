8d8a491498dcbe7f464fc08d25f781d901464293
Saber Hosseini <saber.hosseini@tagmaster.com>
2025-12-08 11:07:11 +0100

    Commiting Assignment 4 part 1 //Saber

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,31 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    // Wait before obtaining mutex
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    
+    // Obtain mutex
+    if (pthread_mutex_lock(thread_func_args->mutex) != 0) {
+        ERROR_LOG("Failed to lock mutex");
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+    
+    // Wait before releasing mutex
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+    
+    // Release mutex
+    if (pthread_mutex_unlock(thread_func_args->mutex) != 0) {
+        ERROR_LOG("Failed to unlock mutex");
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+    
+    thread_func_args->thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +53,27 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    
+    // Allocate memory for thread_data
+    struct thread_data *thread_data = (struct thread_data *)malloc(sizeof(struct thread_data));
+    if (thread_data == NULL) {
+        ERROR_LOG("Failed to allocate memory for thread_data");
+        return false;
+    }
+    
+    // Setup thread_data structure
+    thread_data->mutex = mutex;
+    thread_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_data->wait_to_release_ms = wait_to_release_ms;
+    thread_data->thread_complete_success = false;
+    
+    // Create thread
+    if (pthread_create(thread, NULL, threadfunc, thread_data) != 0) {
+        ERROR_LOG("Failed to create thread");
+        free(thread_data);
+        return false;
+    }
+
+    return true;
 }
 
